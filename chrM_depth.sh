4c64bb364f89168e9fac9d6f7adf3be181109c8a
Mitoomics <147225132+Mitoomics@users.noreply.github.com>
2024-07-01 09:03:15 +0800

    Update chrM_depth.sh
    fix some bugs

--- chrM_depth.sh
+++ chrM_depth.sh
@@ -1,9 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <sample_directory>"
+    exit 1
+fi
+
 sample_dir=$1
-cd ${sample_dir}/fragment_study/mis_10_splitedBy125
-mkdir depth_file
-cd ${sample_dir}/fragment_study/mis_10_splitedBy125/125_splited_sam_file
-fils=$(ls -l *.sam | awk '{if($5>1000) print$9}')
-for s in ${fils}
-do
-    samtools depth -d 500000000 -a -m 0 ${s} > ../depth_file/${s}.txt
+insert_125="fragment_study/mis_10_splitedBy125/125_splited_sam_file"
+depth_dir="depth_file"
+
+cd "${sample_dir}/${insert_125}"
+
+mkdir -p ../${depth_dir}
+
+fils=$(find . -type f -name '*.sam' -size +1000c)
+
+for s in ${fils}; do
+    if samtools depth -d 500000000 -a -m 0 "${s}" > "../${depth_dir}/$(basename ${s}).txt"; then
+        echo "Depth information for $(basename ${s}) has been successfully generated."
+    else
+        echo "Failed to generate depth information for $(basename ${s})."
+    fi
 done
