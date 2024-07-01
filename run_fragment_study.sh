2813cb4b673a9bf3fece9762b38424e05110fd52
Mitoomics <147225132+Mitoomics@users.noreply.github.com>
2024-07-01 10:13:42 +0800

    Update run_fragment_study.sh

--- run_fragment_study.sh
+++ run_fragment_study.sh
@@ -1,10 +1,16 @@
-#!bin/bash
+#!/bin/bash
 start=$(date +%s) #start time
 echo "start time: "`date` 
+
+if [ ! -f sample_path_list.txt ]; then
+   echo "sample_path_list.txt not exist, check again!"
+   exit 1
+fi
+
 sed -i 's/\r//g' sample_path_list.txt
+
 cat sample_path_list.txt | while read line
 do
-
     echo ${line}
     #cal endmotifs
     #python endmotifs_stat.py ${line}
@@ -12,9 +18,9 @@ do
     #python split_mis10_by125.py ${line}
     #cal depth
     #bash chrM_depth.sh ${line}
-    #cal zfsd feature
+    #cal fsd and fsd feature
     #python fsd_compute.py ${line}
-    # cal insertsize
+    # compute insertsize
     #bash insertisize_find_mt.sh ${line}
     #insertsize stat
     python insertsize_stat.py ${line}
